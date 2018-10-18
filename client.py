5bee9ba880edc051df8f79bb46f95e083d0d5477
“pvillaul” <p.villaluca@gmail.com>
2018-10-18 10:39:48 +0200

    Añadido codigo que faltaba

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
-# Authors: Meyer Montagner, Pablo Villalba
-# Programa que funciona como cliente al servidor. Usado como práctica del concepto de API.
+# Developers: Meyer Montagner, Pablo Villalba
+# Scrum Manager: Damaso
+# Programa que funciona como cliente al servidor. Usado como práctica de Scrum.
 
 import requests
 
@@ -7,12 +8,26 @@ import requests
 """ Acceso a endpoint /1a/. 
 Recibe un número como parámetro.
 Retorna un JSON con el cuadrado del número. """
-result1 = requests.get('http://127.0.0.1:5000/1a/?url=https://en.wikipedia.org/wiki/Interdisciplinarity')  # type: requests.Response
+result1 = requests.get("http://127.0.0.1:5000/1a/?url=https://en.wikipedia.org/wiki/Interdisciplinarity")  # type: requests.Response
 print(result1.text)
+
+
+""" Acceso a endpoint /2a/. 
+Recibe un texto como parámetro.
+Retorna un JSON con el texto separado en una lista de listas. """
+result2 = requests.get("http://127.0.0.1:5000/2a/?texto= Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis consectetur justo. Maecenas porta aliquet arcu sed iaculis. Maecenas tincidunt venenatis quam, in posuere libero tempus at. Curabitur porttitor sed lorem a luctus. Nam ut nisl eu odio pretium aliquet at in ante. Fusce euismod venenatis dictum. Ut pulvinar bibendum justo sit amet accumsan. Suspendisse quis orci eget magna eleifend auctor. Sed ultrices turpis sit amet odio aliquet sodales. In hac habitasse platea dictumst. Maecenas sed arcu vel arcu condimentum rhoncus in eget lectus. Aliquam et libero elit. Duis nisl nisl, ullamcorper sed turpis nec, varius volutpat nunc. Pellentesque tincidunt eget magna sit amet efficitur. Mauris lacinia, libero ac imperdiet viverra, nisl magna auctor arcu, sit amet iaculis lorem velit ut nisl. Sed vitae placerat velit. Sed sit amet lorem vel erat fermentum malesuada et quis justo. In vitae laoreet justo. Mauris nisl nibh, gravida vel mi a, elementum condimentum tellus. Nulla et orci id tortor facilisis pharetra. Fusce ac metus in mi iaculis congue consequat quis est. Suspendisse accumsan vehicula elementum. Maecenas dictum tempor eros. Proin ut mauris malesuada, laoreet lorem sit amet, porta metus.")  # type: requests.Response
+print(result2.text)
+
 
 """ Acceso a endpoint /3a/. 
 Recibe un número como parámetro.
 Retorna un JSON con el cuadrado del número. """
-result1 = requests.get("http://127.0.0.1:5000/3a/?value=[[%27HOLA%27],[%27BOBO%27]]")  # type: requests.Response
-print(result1.text)
+result3 = requests.get("http://127.0.0.1:5000/3a/?value=[[%27HOLA%27],[%27BOBO%27]]")  # type: requests.Response
+print(result3.text)
 
+
+""" Acceso a endpoint /4a/. 
+Recibe una lista de listas con strings como parámetro.
+Retorna un JSON con los strings ordenados alfabeticamente en una lista de listas. """
+result4 = requests.get("http://127.0.0.1:5000/3a/?value=[['e', 'i', 'u', 'a', 'o'],['z', 'g', 't', 'b', 'm']]")  # type: requests.Response
+print(result4.text)
--- server.py
+++ server.py
@@ -1,18 +1,22 @@
-# Authors: Meyer Montagner, Pablo Villalba
-# Programa que inicia un servidor usado como práctica del concepto de API.
+# Developers: Meyer Montagner, Pablo Villalba
+# Scrum Manager: Damaso
+# Programa que inicia un servidor usado como práctica de Scrum.
 
-from typing import Dict
+
 from flask import Flask, jsonify, request
 from bs4 import BeautifulSoup
 import urllib.request
 import ast
+import nltk
+
 
 app = Flask(__name__)                           # type: Flask
 
 
-
 @app.route('/1a/', methods=['GET'])
 def webScrap():
+    """Recibe desde la ruta una lista de listas con.
+    Retorna una lista de listas con."""
     url = request.args.get('url')
     webpage = urllib.request.urlopen(url)
     soup = BeautifulSoup(webpage, 'html.parser')
@@ -24,8 +28,26 @@ def webScrap():
     return jsonify(text)
 
 
-@app.route('/3a/')
+@app.route('/2a/', methods=['GET'])
+def get_texto():
+    """Recibe desde la ruta un texto.
+    Retorna una lista de listas con las palabras de cada parrafo en ellas."""
+    texto = request.args.get('texto')                       #type: str
+    #parrafos = [s.strip() for s in texto.splitlines()]
+    parrafos = nltk.sent_tokenize(texto)                    #type: list
+    size = len(parrafos)
+    for x in range(size):
+        #aux = parrafos[x].split()
+        aux = nltk.word_tokenize(parrafos[x])
+        parrafos[x] = aux                                   #type: list[list[str]]
+
+    return jsonify(parrafos)
+
+
+@app.route('/3a/', methods=['GET'])
 def lowerCase():
+    """Recibe desde la ruta una lista de listas con.
+    Retorna una lista de listas con."""
     parameter = request.args.get('value')
     list = ast.literal_eval(parameter)
     for x in range(len(list)):
@@ -34,8 +56,21 @@ def lowerCase():
     return jsonify(list)
 
 
+@app.route('/4a/', methods=['GET'])
+def get_listas():
+    """Recibe desde la ruta una lista de listas con strings desordenados.
+    Retorna una lista de listas con los strings ordenados alfabeticamente."""
+
+    str = request.args.get('listas')        #type: str
+    list = ast.literal_eval(str)            #type: list[list[str]]
+
+    size = len(list)
+    for x in range(size):
+        aux = sorted(list[x])
+        list[x] = aux
+    return jsonify(list)
+
 
 if __name__ == "__main__":
-
     """Inicia la ejecución del servidor."""
     app.run(host='127.0.0.1', port=5000, debug=True)
